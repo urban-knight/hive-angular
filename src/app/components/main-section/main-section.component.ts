8c337c209b3cb0eabc7a5b95c0c31c5390c6a76e
urban-knight <fandul.ant@gmail.com>
2018-12-31 01:39:53 +0200

    enhancement/improve_intro_section

--- src/app/components/main-section/main-section.component.ts
+++ src/app/components/main-section/main-section.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-section',
   templateUrl: './main-section.component.html',
   styleUrls: ['./main-section.component.scss']
 })
-export class MainSectionComponent implements OnInit {
+export class MainSectionComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    const canvas = <HTMLCanvasElement> document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const stars = [], // Array that contains the stars
+      FPS = 30, // Frames per second
+      x = canvas.width; // Number of stars
+
+    // Push stars to array
+    for (let i = 0; i < x; i++) {
+      stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random(),
+        vx: Math.floor(Math.random() * 10) - 5,
+        vy: Math.floor(Math.random() * 10) - 5
+      });
+    }
+
+    // Draw the scene
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.globalCompositeOperation = 'lighter';
+
+      for (let i = 0, _x = stars.length; i < _x; i++) {
+        const s = stars[i];
+
+        ctx.fillStyle = '#dedede';
+        ctx.beginPath();
+        ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    }
+
+    // Update star locations
+    function update() {
+      for (let i = 0, _x = stars.length; i < _x; i++) {
+        const s = stars[i];
+
+        s.x += s.vx / FPS;
+        s.y += s.vy / FPS;
+
+        if (s.x < 0 || s.x > canvas.width) {
+          s.x = -s.x;
+        }
+        if (s.y < 0 || s.y > canvas.height) {
+          s.y = -s.y;
+        }
+      }
+    }
+
+    // Update and draw
+    function tick() {
+      draw();
+      update();
+      requestAnimationFrame(tick);
+    }
+    tick();
+  }
 }
